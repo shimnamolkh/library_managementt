f3d7786c700383036f1e3a177a420d2cb1c39431
shimna <you@example.com>
2022-11-11 16:24:38 +0530

    test commit

---
+++ library_management/library_management/doctype/second/second.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, ghjk and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Second', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ library_management/library_management/doctype/second/test_second.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, ghjk and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSecond(FrappeTestCase):
+	pass
